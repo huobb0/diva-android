4ffe44f1dc0ab4b22f76a3cda19b87a2c070c655
timurxyz <x@secdev.eu>
2021-02-20 03:46:51 +0100

    -- build.gradle fixes, 30

--- app/build/generated/source/buildConfig/debug/jakhar/aseem/diva/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/jakhar/aseem/diva/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "jakhar.aseem.diva";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.029";
+  public static final String VERSION_NAME = "1.030";
 }
